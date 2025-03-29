5fab10b85db4a539a522998b4a0288d091ae5369
Jonathing <me@jonathing.me>
2025-03-29 00:13:56 -0400

    Documentation for Log class

--- log-utils/src/main/java/net/minecraftforge/util/logging/Log.java
+++ log-utils/src/main/java/net/minecraftforge/util/logging/Log.java
@@ -26,27 +26,46 @@ import java.io.PrintStream;
  * </ul>
  */
 public final class Log {
-    /** Whether {@link Level#DEBUG} should be logged to {@link System#out}. */
+    /** The lowest level that should be logged. If {@code null}, all logging is completely disabled. */
     public static Level enabled = Level.INFO;
 
+    /** The stream used for {@link Level#DEBUG}. */
     public static final PrintStream DEBUG = new DelegatePrintStream(Level.DEBUG, System.out);
+    /** The stream used for {@link Level#QUIET}. */
     public static final PrintStream QUIET = new DelegatePrintStream(Level.QUIET, System.out);
+    /** The stream used for {@link Level#INFO}. */
     public static final PrintStream INFO = new DelegatePrintStream(Level.INFO, System.out);
+    /** The stream used for {@link Level#WARN}. */
     public static final PrintStream WARN = new DelegatePrintStream(Level.WARN, System.out);
+    /** The stream used for {@link Level#ERROR}. */
     public static final PrintStream ERROR = new DelegatePrintStream(Level.ERROR, System.err);
     public static final PrintStream FATAL = System.err;
 
     private static byte indent = 0;
 
+    /** Pushes the current indentation level up by one. */
     public static void push() {
         indent++;
     }
 
+    /**
+     * Pops the current indentation level down by one.
+     *
+     * @throws IllegalStateException If the indentation level is already 0
+     */
     public static void pop() {
         if (--indent < 0)
             throw new IllegalArgumentException("Cannot pop logger below 0");
     }
 
+    /**
+     * Gets the {@linkplain PrintStream print stream} for the given {@linkplain Level level}. If the given level is not
+     * {@linkplain #enabled enabled} or is {@code null}, then an empty print stream is returned that ignores all method
+     * calls.
+     *
+     * @param level The level to get the print stream for
+     * @return The print stream
+     */
     public static PrintStream getLog(Level level) {
         return level != null ? level.logger : EmptyPrintStream.INSTANCE;
     }
