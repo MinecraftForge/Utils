8ed7a6604ab84f7d8522c7aaea700dad03671ded
Jonathing <me@jonathing.me>
2025-06-25 14:01:57 -0500

    Rework rules strategy for version.json libs
    Still a work in progress, needs proper testing with older versions
    (1.16.5-).

--- json-data-utils/src/main/java/net/minecraftforge/util/data/json/MinecraftVersion.java
+++ json-data-utils/src/main/java/net/minecraftforge/util/data/json/MinecraftVersion.java
@@ -9,8 +9,13 @@ import org.jetbrains.annotations.Nullable;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 // TODO: [MCMaven][Documentation][MinecraftVersion] What in here is nullable?
 /** Represents a Minecraft version and its artifacts. */
@@ -35,13 +40,35 @@ public class MinecraftVersion {
         List<Lib> libs = new ArrayList<>();
 
         for (Library lib : this.libraries) {
-            if (lib.rules != null && lib.rules.size() != 1)
-                throw new UnsupportedOperationException("Rules were found, but wasn't precisely one??? Rules: " + lib.rules);
+            EnumSet<OS> os = EnumSet.noneOf(OS.class);
+            if (lib.rules != null) {
+                for (Rule rule : lib.rules) {
+                    switch (rule.action) {
+                        case "allow":
+                            if (rule.os == null)
+                                os.addAll(Arrays.asList(OS.WINDOWS, OS.MACOS, OS.LINUX));
+                            else
+                                os.add(rule.os.toOS());
+                            break;
+                        case "disallow":
+                            if (os.isEmpty())
+                                os.addAll(Arrays.asList(OS.WINDOWS, OS.MACOS, OS.LINUX));
 
-            OS os = lib.rules == null ? null : lib.rules.get(0).os.toOS();
+                            //noinspection DataFlowIssue
+                            os.remove(rule.os.toOS());
+                            break;
+                    }
+                }
+            }
             libs.add(new Lib(lib.name, lib.downloads.artifact, os));
-            if (lib.downloads.classifiers != null)
-                lib.downloads.classifiers.forEach((k, v) -> libs.add(new Lib(lib.name + ':' + k, v, os)));
+
+            if (lib.natives != null && lib.downloads.classifiers != null) {
+                lib.natives.forEach((nativeOS, classifier) -> {
+                    LibraryDownload nativeLib = lib.downloads.classifiers.get(nativeOS);
+                    if (nativeLib != null)
+                        libs.add(new Lib(lib.name + ':' + classifier, nativeLib, EnumSet.of(Rule.OS.toOS(nativeOS))));
+                });
+            }
         }
 
         return libs;
@@ -50,12 +77,16 @@ public class MinecraftVersion {
     public static final class Lib {
         public final String coord;
         public final LibraryDownload dl;
-        public final OS os;
+        public final EnumSet<OS> os;
 
-        public Lib(String coord, LibraryDownload dl, OS os) {
+        public Lib(String coord, LibraryDownload dl, EnumSet<OS> os) {
             this.coord = coord;
             this.dl = dl;
             this.os = os;
+        }
+
+        public boolean allows(OS os) {
+            return this.os.isEmpty() || this.os.contains(os);
         }
     }
 
@@ -67,8 +98,10 @@ public class MinecraftVersion {
         public String name;
         /** The downloads provided by the library. */
         public Downloads downloads;
+        /** The natives. */
+        public Map<String, String> natives;
         /** The rules for the library. */
-        public List<Rules> rules;
+        public List<Rule> rules;
     }
 
     /** Represents the downloads for a library. */
@@ -96,15 +129,15 @@ public class MinecraftVersion {
         public String path;
     }
 
-    public static class Rules {
+    public static class Rule {
         public String action;
-        public OS os;
+        public @Nullable OS os;
 
         public static class OS {
             public String name;
 
-            public net.minecraftforge.util.data.OS toOS() {
-                switch (this.name) {
+            public static net.minecraftforge.util.data.OS toOS(String name) {
+                switch (name) {
                     case "windows":
                         return net.minecraftforge.util.data.OS.WINDOWS;
                     case "linux":
@@ -114,6 +147,10 @@ public class MinecraftVersion {
                     default:
                         return net.minecraftforge.util.data.OS.UNKNOWN;
                 }
+            }
+
+            public net.minecraftforge.util.data.OS toOS() {
+                return toOS(this.name);
             }
         }
     }
